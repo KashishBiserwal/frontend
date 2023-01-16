d18cda422373a7ec6e9ed4230cdf037ebc1db887
Kashish <kashish.biserwal@protonmail.com>
2023-01-16 13:27:35 +0530

    header buttons

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Search from './Search'
 import Cart from './Cart'
 import Swipe from './Swipe'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
@@ -18,8 +19,12 @@ export default function Header() {
           <Swipe />
         </div>
         <div className='bottom'>
-          <div>Dropdown buttons</div>
-          <div>Login</div>
+          <div>
+            <Link to="products"><button className='header-button products-button'>Products</button></Link>
+          </div>
+          <div>
+            <Link to="login"><button className='header-button login-button'>Login</button></Link>
+          </div>
         </div>
       </div>
     </div>
